587bd60d9253beec230993ad6c931dc264d80c3b
visko505 <mmbb04179@gmail.com>
2026-01-30 18:08:24 +0500

    Update track.js

--- netlify/functions/track.js
+++ netlify/functions/track.js
@@ -1,6 +1,18 @@
+const fetch = require('node-fetch');
+
 exports.handler = async (event) => {
   const ip = event.headers['x-forwarded-for']?.split(',')[0] || 'unknown';
   const time = new Date().toISOString();
-  console.log(`${time} | IP: ${ip}`); // Выводится прямо в Netlify логах
+  let geo = 'Геолокация не определена';
+
+  try {
+    const res = await fetch(`http://ip-api.com/json/${ip}?fields=country,regionName,city,query`);
+    const data = await res.json();
+    geo = `Страна: ${data.country}, Регион: ${data.regionName}, Город: ${data.city} (IP: ${data.query})`;
+  } catch (e) {
+    geo = 'Ошибка определения геолокации';
+  }
+  
+  console.log(`${time} | IP: ${ip} | ${geo}`);
   return { statusCode: 200 };
 };
