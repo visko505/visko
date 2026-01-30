e983651dccdeec52519e9647ae62c0a081324af1
visko505 <mmbb04179@gmail.com>
2026-01-30 18:14:14 +0500

    Update track.js

--- netlify/functions/track.js
+++ netlify/functions/track.js
@@ -3,16 +3,20 @@ const fetch = require('node-fetch');
 exports.handler = async (event) => {
   const ip = event.headers['x-forwarded-for']?.split(',')[0] || 'unknown';
   const time = new Date().toISOString();
-  let geo = 'Геолокация не определена';
+  let geo = '';
 
   try {
     const res = await fetch(`http://ip-api.com/json/${ip}?fields=country,regionName,city,query`);
     const data = await res.json();
-    geo = `Страна: ${data.country}, Регион: ${data.regionName}, Город: ${data.city} (IP: ${data.query})`;
+    if (data && data.country) {
+      geo = `Страна: ${data.country}, Регион: ${data.regionName}, Город: ${data.city} (IP: ${data.query})`;
+    } else {
+      geo = 'Не удалось получить геолокацию';
+    }
   } catch (e) {
     geo = 'Ошибка определения геолокации';
   }
-  
+
   console.log(`${time} | IP: ${ip} | ${geo}`);
-  return { statusCode: 200 };
+  return { statusCode: 200, body: "ok" };
 };
